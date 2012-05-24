2d324953e802879c9970bd19632b39f6d6880ebb
Eric Kim <erickim555@gmail.com>
2012-05-24 15:21:37 -0700

    Added the numpy include path (for access to <numpy/araryobject.h>) to setup.py, for systems that were having trouble installing setup.py (i.e. me, with my Windows 7 machine).

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
 from distutils.core import *
+import numpy as np
 
 setup(name='lineDetect',
       version='1.0',
-      ext_modules=[Extension('lineDetect', ['lineDetectmodule.c'])]
+      ext_modules=[Extension('lineDetect', ['lineDetectmodule.c'])],
+      include_dirs = [np.get_include()]
      )
